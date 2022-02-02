2036003c115b28d4d126bf84fdae93917f731180
Pablo <pablo.aguirredesouza@ebgames.com>
2022-02-02 14:52:09 +1000

    Add project files.

---
+++ todo/Controllers/TodoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using todo.Models;
+
+namespace todo.Controllers
+{
+    public class TodoController: Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public void Insert(Todo todo)
+        {
+            using (SqlConnection con =
+                   new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;Initial Catalog=Todo"))
+            {
+                using (var tableCmd = con.CreateCommand())
+                {
+                    con.Open();
+                    tableCmd.CommandText = $"INSERT INTO todo (name) VALUES ('{todo.Name}')";
+                    try
+                    {
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ todo/Models/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace todo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
